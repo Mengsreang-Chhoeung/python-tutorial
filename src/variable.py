ea51126a5c01ee367212413224304c586149f945
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2023-05-05 19:28:00 +0700

    teaching about how to create a variable

---
+++ src/variable.py
@@ -0,0 +1,19 @@
+greeting = "Hello my name is kok and nowaday I study at NUM"
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+print(greeting)
+
+greeting = 168
+print(greeting)
+print(greeting)
+print(greeting)
