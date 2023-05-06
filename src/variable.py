e4013b87baa845f0e3c7957b4d9238c376c8d1ea
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2023-05-06 22:34:02 +0700

    teaching about casting variable

--- src/variable.py
+++ src/variable.py
@@ -1,19 +1,34 @@
-greeting = "Hello my name is kok and nowaday I study at NUM"
-print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
-print(greeting)
+# Creating variable
+# greeting = "Hello my name is kok and nowaday I study at NUM"
+# print(greeting)
+# print(greeting)
+# print(greeting)
+# print(greeting)
+# print(greeting)
+# print(greeting)
+# print(greeting)
+# print(greeting)
+# print(greeting)
+# print(greeting)
+# print(greeting)
+# print(greeting)
+# print(greeting)
 
-greeting = 168
+# greeting = 168
+# print(greeting)
+# print(greeting)
+# print(greeting)
+
+
+# Casting variable
+greeting = str("168")
+x = int(123)
+y = float(3.6)
+t = bool(True)
 print(greeting)
-print(greeting)
-print(greeting)
+print(x)
+print(y)
+print(t)
+
+# greeting = 12
+# print(greeting)
